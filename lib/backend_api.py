2e37fd0fbcf81a26d8c0f0fb888556145ed815a8
nedved <nadavleder@gmail.com>
2023-06-01 18:39:20 +0300

    changes to backend_api.py

--- lib/backend_api.py
+++ lib/backend_api.py
@@ -7,8 +7,18 @@ from lib import dependency_graph
 
 
 class ServerSide:
-    @staticmethod
-    def get_suggestions(code, preferences):
+    def __init__(self, code=None):
+        if code is not None:
+            self.code_ = code
+            self.functions_ = dependency_graph.build_topological_sort(code)
+            self.ast_tree_ = ast.parse(code)
+
+    def set_code(self, code):
+        self.code_ = code
+        self.functions_ = dependency_graph.build_topological_sort(code)
+        self.ast_tree_ = ast.parse(code)
+
+    def get_suggestions(self, preferences):
         """
         The function will recieve a python code string and return a list of suggestions based on the preferences
             specified.
@@ -17,24 +27,21 @@ class ServerSide:
         :return: A dictinary of suggestions, each key is a function name and the value is a list of FunctionImprovements
          objects
         """
-        suggestions = dict()
+        suggestions = {}
 
         if preferences['style']:
-            suggestions = generate_coding_style.get_suggestions(code, preferences['style'])
+            suggestions = generate_coding_style.get_suggestions(self.code_, preferences['style'])
 
         if preferences['tests']:
-            suggestions = generate_tests.get_suggestions(code, preferences['tests'])
+            suggestions = generate_tests.get_suggestions(self.code_, preferences['tests'])
 
         return suggestions
 
-    @staticmethod
-    def get_function_source(source_code, function_name, tree=None):
-        if tree is None:
-            tree = ast.parse(source_code)
+    def get_function_source(self, function_name):
         # Get the start and end line numbers of the function definition
         function_node = None
         function_index = 0
-        for index, node in enumerate(tree.body):
+        for index, node in enumerate(self.ast_tree_.body):
             if isinstance(node, ast.FunctionDef) and node.name == function_name:
                 function_node = node
                 function_index = index
@@ -44,25 +51,21 @@ class ServerSide:
 
         start_line = function_node.lineno
         # Extract the lines containing the function
-        lines = source_code.split('\n')
+        lines = self.code_.split('\n')
 
-        if function_index == len(tree.body) - 1:
+        if function_index == len(self.ast_tree_.body) - 1:
             function_lines = lines[start_line - 1:]
         else:
-            function_lines = lines[start_line - 1: tree.body[function_index + 1].lineno - 1]
+            function_lines = lines[start_line - 1: self.ast_tree_.body[function_index + 1].lineno - 1]
         # Join the lines back into a string
         function_source = '\n'.join(function_lines)
         return function_source
 
-    @staticmethod
-    def get_sources(code):
+    def get_sources(self):
         """
         Gets source code and returns source codes of functions by topological sort order.
-        :param code:
         :return:
         """
-        functions = dependency_graph.build_topological_sort(code)
-        ast_tree = ast.parse(code)
-        for function_name in reversed(functions):
-            function_source = ServerSide.get_function_source(code, function_name, tree=ast_tree)
+        for function_name in reversed(self.functions_):
+            function_source = ServerSide.get_function_source(self.code_, function_name)
             yield function_source
