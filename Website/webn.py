8264bcd130e97fc7b9c1922c956d640c6e3eb674
Netanel Magzimof <netanel.magzimof@mail.huji.ac.il>
2023-06-01 23:51:41 +0300

    started to work on the website

--- Website/webn.py
+++ Website/webn.py
@@ -1,53 +1,102 @@
 import streamlit as st
-
-st.title("Review My Code")
-
-ch_file = st.empty()
-code_data = ch_file.file_uploader("Upload Code")
+import time
 
 
-def call_api():
-    f = open("result.txt", "w")
-    f.write("hellon")
-    return f
+global code_data
 
-result = []
-counter = 0
-
-def start():
-    result = call_api()
-    counter = 0
-
-def foo():
-    print("Hi")
+if 'key' not in st.session_state:
+    st.session_state.key = 'UploadFile'
 
 
-ch1 = st.empty()
-ch2 = st.empty()
-ch3 = st.empty()
-ch4 = st.empty()
-checkbox1 = ch1.checkbox("cb1")
-checkbox2 = ch2.checkbox("cb2")
-checkbox3 = ch3.checkbox("cb3")
-checkbox4 = ch4.checkbox("cb4")
-
-placeholder = st.empty()
-start_button = placeholder.button("Start", on_click=start)
-if start_button:
-    placeholder.empty()
-    ch1.empty()
-    ch2.empty()
-    ch3.empty()
-    ch4.empty()
-    ch_file.empty()
-
-code = '''def hello():
-    print("hello")'''
+def call_api_improve():
+    pass
 
 
-user_code = st.code(code, language='python', line_numbers=True)
-accept_yours = st.button("accept yours")
-accept_theirs = st.button("accept theirs")
-suggested_code = st.code(code, language='python', line_numbers=True)
-user_code.code("ithh")
+def call_api_test():
+    pass
+
+
+def run_improve():
+    loading = st.empty()
+    loading.write('Loading...')
+    time.sleep(2)
+    call_api_improve()
+    loading.empty()
+    st.session_state.key = 'Improve'
+
+
+def run_testing():
+    loading = st.empty()
+    loading.write('Loading...')
+    time.sleep(2)
+    call_api_test()
+    loading.empty()
+    st.session_state.key = 'TestResults'
+
+
+st.title("Review :blue[Your] Code")
+
+if st.session_state.key == 'UploadFile':
+    ch_text = st.empty()
+    ch_text.write("First We need to see your code")
+    ch_upload = st.empty()
+    code_data = ch_upload.file_uploader("Upload Code")
+    if code_data:
+        st.session_state.key = 'ChooseOperation'
+        ch_text.empty()
+        ch_upload.empty()
+
+
+if st.session_state.key == 'ChooseOperation':
+
+    # Create an empty placeholder for the start button
+    ch_improve_button = st.empty()
+    ch_test_button = st.empty()
+
+    # Create the start button and assign the 'start' function to its 'on_click' parameter
+    improve_button = ch_improve_button.button("Improve My Code")
+    test_button = ch_test_button.button("Test My Code")
+
+    # If the start button is clicked, clear the placeholders
+    if improve_button or test_button:
+        ch_improve_button.empty()
+        ch_test_button.empty()
+        st.session_state.key = 'loading'
+        if improve_button:
+            run_improve()
+        else:
+            run_testing()
+
+
+
+if st.session_state.key == 'runningApi':
+    # Define the initial code string
+    pass
+
+
+if st.session_state.key == 'Improve':
+    code = '''def hello():
+            print("hello")'''
+    # Display the user's code with syntax highlighting
+    user_code = st.code(code, language='python', line_numbers=True)
+
+    # Create empty placeholders for the 'accept yours' and 'accept theirs' buttons
+    ch_accept_yours = st.empty()
+    ch_accept_theirs = st.empty()
+
+    # Create the 'accept yours' and 'accept theirs' buttons
+    accept_yours = ch_accept_yours.button("Accept Yours")
+    accept_theirs = ch_accept_theirs.button("Accept Theirs")
+
+    # Display the suggested code with syntax highlighting
+    suggested_code = st.code(code, language='python', line_numbers=True)
+
+if st.session_state.key == 'TestResults':
+    pass
+
+
+
+
+
+
 
